5c36bcec12eb1cf6b159d1c98169155683c3de3c
Heroturtle <super@heroturtle.com>
2015-01-11 23:11:22 +0100

    first week

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,31 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+  end
+
+  def edit
 
   end
+
+  def update
+
+  end
+
+  private
+
+    def post_params
+      params(:post).require(:title, :url, :description)
+    end
 end
